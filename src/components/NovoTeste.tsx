6fe6b9b96d9c6d6a33e4082758d9d1fe0314eb1e
Gustavo Amaral <56303449+GussCloud@users.noreply.github.com>
2025-02-14 17:29:19 -0300

    Adicionando o componente NovoTeste

---
+++ src/components/NovoTeste.tsx
@@ -0,0 +1,15 @@
+```tsx
+// src/components/NovoTeste.tsx
+import React from "react";
+import Layout from "./Layout";
+
+const NovoTeste: React.FC = () => {
+  return (
+    <Layout>
+      <h1 className="text-2xl font-bold">IA Testasda</h1>
+    </Layout>
+  );
+};
+
+export default NovoTeste;
+```
